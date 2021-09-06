cb1dda01af64db66fa9bf6a15ecc3d9b965062a3
Liliia klachko <liliannaeverdin@gmail.com>
2021-09-06 22:28:58 +0300

    created markup for cards

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,2 +1,10 @@
 import menuData from '../menu.json'
 import menuCardTemplate from '../templates/menu.hbs'
+
+
+console.log(menuCardTemplate(menuData))
+
+const menu = document.querySelector('.js-menu')
+ 
+const menuMarkup = menuCardTemplate(menuData)
+menu.insertAdjacentHTML('beforeend', menuMarkup)
