0c454eddbc813263328e2b43a84138e302e1c3ae
Liliia klachko <liliannaeverdin@gmail.com>
2021-09-07 23:04:27 +0300

    added switch heme

--- src/js/menu.js
+++ src/js/menu.js
@@ -8,3 +8,42 @@ const menu = document.querySelector('.js-menu')
  
 const menuMarkup = menuCardTemplate(menuData)
 menu.insertAdjacentHTML('beforeend', menuMarkup)
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const { LIGHT, DARK } = Theme
+
+const checkbox = document.getElementById('theme-switch-toggle')
+const body = document.body;
+
+checkbox.addEventListener("change", onCheckboxChecked)
+// checkbox.addEventListener("toggle", onCheckboxToggle)
+// function onCheckboxToggle(evt) {
+//      evt.checked = true
+//  }
+
+body.classList.add(window.localStorage.getItem('currentTheme'));
+
+function onCheckboxChecked() {
+
+    if (checkbox.checked) {
+        body.classList.remove(LIGHT)
+        body.classList.add(DARK)
+        window.localStorage.setItem('currentTheme', DARK);
+        // window.localStorage.setItem('checkbox', checkbox.checked = true);
+
+        // checkbox.setAttribute('checked')
+
+    }
+    else {
+        body.classList.remove(DARK)
+        body.classList.add(LIGHT)
+        window.localStorage.setItem('currentTheme', LIGHT);
+        // window.localStorage.setItem('checkbox', checkbox.checked != true);
+
+
+
+    }
+}
